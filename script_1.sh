cf52b16761b976b6adfe9efb613bc85ceb1a29ab
Mujahid <mujali29@in.ibm.com>
2017-05-24 07:09:23 -0400

    Script1 for verify tag
    Change-Id: Ib637e733fca9a683e6bce6e691daab4ed126753a

---
+++ script_1.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "${GIT_COMMIT}"
+echo "Ali"
+echo "${GERRIT_PATCHSET_REVISION}"
+if [[ ! -z "${GERRIT_PATCHSET_REVISION}" ]]
+then
+  echo "Inside if block:"
+  ssh -p 29418 mujali29@jupiter-vm288.pok.stglabs.ibm.com gerrit review --verified +1 ${GERRIT_PATCHSET_REVISION}
+fi  
+
